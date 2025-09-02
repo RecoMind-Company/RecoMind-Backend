7112cfc8f9704642bee8d849e3df18cce713733e
Reda Saad <Reda.Saad@gmail.com>
2025-09-02 21:33:36 +0300

    Add project files.

---
+++ Core/Model/CodeVerification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class CodeVerification
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ ProjectApi/Program.cs
@@ -0,0 +1,138 @@
+
+using Core.Interfaces;
+using Core.Model;
+using Core.Services;
+using Core.Settings;
+using Infrastructure;
+using Infrastructure.Authentication;
+using Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
+namespace RecoMind
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContext<AppDBContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration["ConnectionStrings:ProdcutionConnection"],
+                    options => options.MigrationsAssembly(typeof(AppDBContext).Assembly.FullName));
+            });
+
+
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("jwt"));
+            builder.Services.Configure<EmailConfgSettings>(builder.Configuration.GetSection("email-confg"));
+            builder.Services.AddScoped<CodeDatabaseServices>();
+            builder.Services.AddScoped<SendEmailServices>();
+            builder.Services.AddScoped<GenerateCodeVerify>();
+
+
+            builder.Services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+
+            builder.Services.AddScoped<PasswordHasher<AppUser>>();
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDBContext>();
+
+            builder.Services.AddTransient<IAuthentication, Authentication>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"]))
+
+                };
+
+            });
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                });
+
+                options.AddSecurityRequirement(securityRequirement: new OpenApiSecurityRequirement
+                {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Name = "Bearer",
+                                In = ParameterLocation.Header
+                            },
+                            new List<string>()
+                        }
+                });
+            });
+
+
+
+
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
