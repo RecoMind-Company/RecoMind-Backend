2488654043027aeb34e2412cbb9358b51ca7c79f
BassimEhab <bassimstudy@gmail.com>
2025-11-23 12:57:17 +0200

    Add gRPC services for account management
    Introduced new gRPC services and message definitions for account management, including profile retrieval, update, and deletion.
    
    - Updated `AccountMessages.proto` and `AccountService.proto` to define messages (`ProfileMessage`, `BaseMessage`, etc.) and RPC methods (`GetProfile`, `UpdateProfile`, `DeleteUser`).
    - Implemented `GrpcAccountService` with authentication and file upload handling using `GrpcFormFile`.
    - Added `GrpcFormFile` class to support file uploads in gRPC.
    - Updated `RecoMindAuthenticationAPI.csproj` to include new protobuf files in the `<Protobuf>` item group.

---
+++ RecoMindAuthentication/RecoMindAuthenticationAPI/Grpc/Account/Service/GrpcFormFile.cs
@@ -0,0 +1,32 @@
+﻿
+namespace RecoMindAuthenticationAPI.Grpc.Account.Service;
+
+public class GrpcFormFile : IFormFile
+{
+    private readonly byte[] _content;
+    private readonly string _fileName;
+    public GrpcFormFile(byte[] content, string fileName)
+    {
+        _content = content;
+        _fileName = fileName;
+    }
+    public long Length => _content.Length;
+    public string FileName => _fileName;
+
+    #region Not Implemented
+    public string ContentType => throw new NotImplementedException();
+
+    public string ContentDisposition => throw new NotImplementedException();
+
+    public IHeaderDictionary Headers => throw new NotImplementedException();
+    public string Name => throw new NotImplementedException();
+    #endregion
+
+    public Stream OpenReadStream() => new MemoryStream(_content);
+    public void CopyTo(Stream target) => OpenReadStream().CopyTo(target);
+    public async Task CopyToAsync(Stream target, CancellationToken cancellationToken = default)
+    {
+        await OpenReadStream().CopyToAsync(target, cancellationToken);
+    }
+
+}
