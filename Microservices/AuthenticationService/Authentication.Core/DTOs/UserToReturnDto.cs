58c848da71393f58fa40609794996793bfe79388
BassimEhab <bassimstudy@gmail.com>
2025-12-03 23:49:15 +0200

    Authentication service: Add user retrieval functionality via gRPC services
    Introduced DTOs `UserToReturnDto` and `UsersToReturnDto` to represent user data. Updated `IAuthenticationService` with `GetUserById` and `GetUsersByIds` methods, and implemented these in `AuthenticationService`.
    
    Modified `AuthenticationMessages.proto` and `AuthenticationService.proto` to define gRPC messages and RPCs for user retrieval. Updated `GrpcAuthenticationService` to handle these RPCs.

---
+++ Microservices/AuthenticationService/Authentication.Core/DTOs/UserToReturnDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Authentication.Core.DTOs;
+
+public class UserToReturnDto
+{
+    public string Id { get; set; } = default!;
+    public string Role { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
