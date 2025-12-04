6bc20b97d1fe32cf6e6769b8c93848e9d7bbf48d
BassimEhab <bassimstudy@gmail.com>
2025-12-04 20:30:13 +0200

    Report service:: Add JWT authentication and enhance Swagger security
    Introduced JWT-based authentication using `JwtBearerDefaults`
    and configured token validation parameters (Issuer, Audience,
    SecretKey) from `JwtOptions`. Enhanced Swagger documentation
    by adding a `BearerAuth` security scheme and enforcing its
    use via security requirements. Replaced default Swagger setup
    with detailed configuration. Retained existing gRPC client
    setup while adding authentication and Swagger improvements.

--- Microservices/ReportService/WebApi/Extensions/ApiServicesExtention.cs
+++ Microservices/ReportService/WebApi/Extensions/ApiServicesExtention.cs
@@ -1,4 +1,8 @@
 ﻿using Infrastructure.gRPC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace WebApi.Extensions;
 
@@ -8,7 +12,47 @@ public static class ApiServicesExtention
     {
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(cfg =>
+        {
+            cfg.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+            });
+            cfg.AddSecurityRequirement(new OpenApiSecurityRequirement
+               {
+                   {
+                   new OpenApiSecurityScheme
+                       {
+                           Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "BearerAuth" }
+                       },
+                       []
+                   }
+               });
+        });
+        builder.Services.AddAuthentication(config =>
+        {
+            config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["JwtOptions:Issuer"],
+                ValidAudience = configuration["JwtOptions:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:SecretKey"])),
+                ClockSkew = TimeSpan.Zero, // ONLY FOR TESTING
+            };
+        });
         builder.Services.AddGrpcClient<TeamGrpcService.TeamGrpcServiceClient>(o =>
         {
             o.Address = new Uri(configuration["Urls:TeamServiceUrl"]);
