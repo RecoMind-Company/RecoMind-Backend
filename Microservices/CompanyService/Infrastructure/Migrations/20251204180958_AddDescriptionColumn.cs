fbe5b290fb18726d72a45d71eb7d4047cc3adc3c
BassimEhab <bassimstudy@gmail.com>
2025-12-04 23:39:48 +0200

    commit

---
+++ Microservices/CompanyService/Infrastructure/Migrations/20251204180958_AddDescriptionColumn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Companies");
+        }
+    }
+}
