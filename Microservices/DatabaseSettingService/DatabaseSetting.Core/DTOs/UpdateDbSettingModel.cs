fdaa72d2dac44744546b75aab05fe686771cf4da
ahmedMo77 <a7medmo25@gmail.com>
2025-12-05 19:25:07 +0200

    Add TestAiService & Update Team and DbSetting

--- Microservices/DatabaseSettingService/DatabaseSetting.Core/DTOs/UpdateDbSettingModel.cs
+++ Microservices/DatabaseSettingService/DatabaseSetting.Core/DTOs/UpdateDbSettingModel.cs
@@ -11,6 +11,10 @@ namespace DatabaseSetting.Core.DTOs
     {
         public string Name { get; set; }
         public string DbType { get; set; }
-        public string ConnectionString { get; set; }
+
+        public string Server { get; set; }
+        public string DbName { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
     }
 }
