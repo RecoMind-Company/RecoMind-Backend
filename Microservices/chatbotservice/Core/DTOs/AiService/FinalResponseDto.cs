b32706834790de3372f986fe28545312955efe87
Hassan Atia <hatia1248@gmail.com>
2025-12-23 00:07:24 +0200

    commint

--- Microservices/chatbotservice/Core/DTOs/AiService/FinalResponseDto.cs
+++ Microservices/chatbotservice/Core/DTOs/AiService/FinalResponseDto.cs
@@ -16,15 +16,17 @@ namespace Core.DTOs.AiService
         public string TaskId { get; set; }
         [JsonPropertyName("status")]
         public string Status { get; set; }
+
         [JsonPropertyName("result")]
+        [JsonConverter(typeof(ResponseMessageConverter))]
         public ResponseMessage Result { get; set; } = new ResponseMessage();
     }
 
     public class ResponseMessage
     {
         [JsonPropertyName("sql_query")]
-        public string Sql_Query { get; set; }
+        public string? Sql_Query { get; set; }
         [JsonPropertyName("answer")]
-        public string Answer { get; set; }
+        public string? Answer { get; set; }
     }
 }
--- Microservices/chatbotservice/Core/Services/ChatBotService.cs
+++ Microservices/chatbotservice/Core/Services/ChatBotService.cs
@@ -98,8 +98,9 @@ namespace Core.Services
 
         public async Task SaveToDatabase(SaveDto model)
         {
-            var charmessage = _mapper.Map<ChatMessage>(model);
-            await _unitOfWork.Entity.AddAsync(charmessage);
+            var chatmessage = _mapper.Map<ChatMessage>(model);
+            chatmessage.Id = Guid.NewGuid().ToString();
+            await _unitOfWork.Entity.AddAsync(chatmessage);
             await _unitOfWork.Save();
         }
     }
