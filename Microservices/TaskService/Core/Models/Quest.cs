60bd082a5dc2a1a6960af49cac885cc2479c1fba
BassimEhab <bassimstudy@gmail.com>
2026-03-05 18:56:20 +0200

    feat(TaskService): Add Quest, QuestStatusEnum, and UserQuests models.
    Introduced Quest, QuestStatusEnum, and UserQuests classes in Core.Models. Quest models quest details and assignments, QuestStatusEnum defines quest states, and UserQuests links quests to users.

---
+++ Microservices/TaskService/Core/Models/Quest.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Models;
+
+public class Quest
+{
+    public string QuestId { get; set; } = default!;
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public QuestStatusEnum Status { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime DeadLine { get; set; }
+    public TimeSpan Duration => DeadLine - StartDate;
+    public string PlanId { get; set; } = default!;
+    public ICollection<UserQuests> UserAssignedQuests { get; set; } = [];
+}
---
+++ Microservices/TaskService/Core/Models/UserQuests.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models;
+
+public class UserQuests
+{
+    public string QuestId { get; set; } = default!;
+    public string UserId { get; set; } = default!;
+    public Quest Quest { get; set; } = default!;
+}
