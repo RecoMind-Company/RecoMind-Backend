30f4654fc7c090f12fe004989addae32cccf1998
BassimEhab <bassimstudy@gmail.com>
2026-03-05 18:59:40 +0200

    feat(TaskService): Add ApplicationDbContext, and Inial migration.
    Added ApplicationDbContext with Quests and UserQuests tables, including composite key and relationships. Registered DbContext with SQL Server in Program.cs. Added initial migration and model snapshot.

---
+++ Microservices/TaskService/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Quest> Quests { get; set; }
+    public DbSet<UserQuests> UserQuests { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserQuests>()
+            .HasKey(uq => new { uq.UserId, uq.QuestId });
+    }
+}
---
+++ Microservices/TaskService/Infrastructure/Migrations/20260305165055_InitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quests",
+                columns: table => new
+                {
+                    QuestId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeadLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PlanId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quests", x => x.QuestId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserQuests",
+                columns: table => new
+                {
+                    QuestId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserQuests", x => new { x.UserId, x.QuestId });
+                    table.ForeignKey(
+                        name: "FK_UserQuests_Quests_QuestId",
+                        column: x => x.QuestId,
+                        principalTable: "Quests",
+                        principalColumn: "QuestId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserQuests_QuestId",
+                table: "UserQuests",
+                column: "QuestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserQuests");
+
+            migrationBuilder.DropTable(
+                name: "Quests");
+        }
+    }
+}
