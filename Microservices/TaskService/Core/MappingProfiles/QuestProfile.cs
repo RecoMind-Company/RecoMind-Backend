4fc43c89e5e520d6e2a7bb0d4a9f0c27de0740d2
BassimEhab <bassimstudy@gmail.com>
2026-03-06 22:38:48 +0200

    feat(TaskService): Add mapping from UserQuests to QuestToReturnDto.
    Introduced a new AutoMapper configuration in QuestProfile to map UserQuests to QuestToReturnDto, extracting properties from the nested Quest object and mapping UserAssignedQuests by UserId.

--- Microservices/TaskService/Core/MappingProfiles/QuestProfile.cs
+++ Microservices/TaskService/Core/MappingProfiles/QuestProfile.cs
@@ -24,6 +24,19 @@ public class QuestProfile : Profile
             .ForMember(des => des.UserAssignedQuests, opt => opt.
             MapFrom(src => src.UserAssignedQuests.Select(x => x.UserId).ToList()));
 
+        CreateMap<UserQuests, QuestToReturnDto>()
+            .ForMember(des => des.QuestId, opt => opt.MapFrom(src => src.Quest.QuestId))
+            .ForMember(des => des.Title, opt => opt.MapFrom(src => src.Quest.Title))
+            .ForMember(des => des.Description, opt => opt.MapFrom(src => src.Quest.Description))
+            .ForMember(des => des.Status, opt => opt.MapFrom(src => src.Quest.Status))
+            .ForMember(des => des.StartDate, opt => opt.MapFrom(src => src.Quest.StartDate))
+            .ForMember(des => des.DeadLine, opt => opt.MapFrom(src => src.Quest.DeadLine))
+            .ForMember(des => des.Duration, opt => opt.MapFrom(src => src.Quest.Duration))
+            .ForMember(des => des.PlanId, opt => opt.MapFrom(src => src.Quest.PlanId))
+
+            .ForMember(des => des.UserAssignedQuests, opt => opt.
+            MapFrom(src => src.Quest.UserAssignedQuests.Select(x => x.UserId).ToList()));
+
         CreateMap<UpdateQuestDto, Quest>()
             .ForAllMembers(opt => opt.Condition((src, des, srcMember) => srcMember != null));
     }
