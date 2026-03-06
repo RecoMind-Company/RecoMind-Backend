b323762f7d361b6d4179eee99585aa572f0c1748
BassimEhab <bassimstudy@gmail.com>
2026-03-06 02:13:18 +0200

    feat(TaskService): Add QuestProfile mapping with custom logic for QuestDto.
    Introduced QuestProfile using AutoMapper to map between QuestDto and Quest, including custom logic for StartDate and Status properties. Also added mapping from Quest to QuestToReturnDto.

---
+++ Microservices/TaskService/Core/MappingProfiles/QuestProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Core.Dtos;
+using Core.Models;
+
+namespace Core.MappingProfiles;
+
+public class QuestProfile : Profile
+{
+    public QuestProfile()
+    {
+        CreateMap<QuestDto, Quest>()
+            .ForMember(des => des.StartDate, opt => opt.
+            MapFrom(src => src.StartDate ?? DateTime.UtcNow))
+
+            .ForMember(des => des.Status, opt => opt.
+            MapFrom(src => src.Status != null
+            ? (QuestStatusEnum)src.Status
+            : (src.StartDate ?? DateTime.UtcNow).Date == DateTime.UtcNow.Date
+            ? QuestStatusEnum.active
+            : QuestStatusEnum.pending));
+
+
+        CreateMap<Quest, QuestToReturnDto>();
+    }
+
+
+}
