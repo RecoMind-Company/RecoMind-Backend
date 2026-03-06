40a6518df7272c9ecfb747cdb744c4d03dce9c80
BassimEhab <bassimstudy@gmail.com>
2026-03-06 02:20:35 +0200

    feat(TaskService): Enhance DI setup and JSON config in Program.cs.

--- Microservices/TaskService/WebApi/Program.cs
+++ Microservices/TaskService/WebApi/Program.cs
@@ -1,18 +1,28 @@
+using Core.Interfaces;
+using Core.MappingProfiles;
+using Core.Services;
+using Core.ServicesAbstractions;
+using FluentValidation;
 using Infrastructure.Context;
+using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApi.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddJsonOptions(jo => jo.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
+builder.Services.AddAutoMapper(cfg => { }, typeof(QuestProfile).Assembly);
+builder.Services.AddValidatorsFromAssembly(typeof(QuestDtoValidator).Assembly);
+builder.Services.AddScoped<IQuestService, QuestService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
