d719fca2d77c2c789c6199493bdf70a8a82ae306
BassimEhab <bassimstudy@gmail.com>
2026-03-06 02:08:55 +0200

    feat(TaskService): Apply Result pattern with Error, and QuestErrors classes.
    Introduce Result folder with three new files:
    - Error.cs: defines Error record for error handling.
    - QuestErrors.cs: adds placeholder class for quest-related errors.
    - Result.cs: implements generic Result<T> for encapsulating operation outcomes, supporting both success and failure states with helper methods.

---
+++ Microservices/TaskService/Core/Result/QuestErrors.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.Result;
+
+public class QuestErrors
+{
+}
---
+++ Microservices/TaskService/Core/Result/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Result;
+
+public class Result<T>
+{
+    private Result(T value) => Value = value;
+    private Result(IEnumerable<Error> errors) => ErrorList = errors;
+
+
+    public T? Value { get; set; }
+    public IEnumerable<Error> ErrorList { get; set; } = [];
+    public bool IsSuccess => !ErrorList.Any();
+    public static Result<T> Success(T value) => new Result<T>(value);
+    public static Result<T> Failure(Error error) => new Result<T>(new List<Error> { error });
+    public static Result<T> Failure(IEnumerable<Error> errors) => new Result<T>(errors);
+    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onFailure) => IsSuccess ? onSuccess(Value!) : onFailure(ErrorList);
+}
