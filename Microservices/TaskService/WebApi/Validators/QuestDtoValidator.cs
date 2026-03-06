9a36dca5c6b5b35c1e4a7df155df598ce7c353be
BassimEhab <bassimstudy@gmail.com>
2026-03-06 02:16:00 +0200

    feat(TaskService): Add QuestDtoValidator with FluentValidation integration.
    Added QuestDtoValidator to enforce validation rules on QuestDto using FluentValidation.

---
+++ Microservices/TaskService/WebApi/Validators/QuestDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using Core.Dtos;
+using FluentValidation;
+
+namespace WebApi.Validators
+{
+    public class QuestDtoValidator : AbstractValidator<QuestDto>
+    {
+        public QuestDtoValidator()
+        {
+            RuleFor(q => q.Title)
+                .NotEmpty()
+                .WithMessage("Title is required.")
+                .MaximumLength(100)
+                .WithMessage("Title must not exceed 100 characters.");
+
+            RuleFor(q => q.Description)
+                .MaximumLength(255)
+                .When(q => !string.IsNullOrEmpty(q.Description));
+
+            RuleFor(q => q.Status)
+                .InclusiveBetween(0, 3)
+                .When(q => q.Status is not null);
+
+            RuleFor(q => q.StartDate)
+                .Must(date => date >= DateTime.UtcNow.AddMinutes(-2)).WithMessage("Start date must be in the future.")
+                .When(q => q.StartDate is not null);
+
+            RuleFor(q => q.DeadLine)
+                .NotEmpty().WithMessage("Deadline is required.")
+                .Must(date => date >= DateTime.UtcNow.AddMinutes(-2)).WithMessage("Deadline must be in the future.");
+        }
+    }
+}
