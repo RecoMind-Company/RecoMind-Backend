53cc976eeff4515e19d0ee2cc8a01f467a0cc78d
BassimEhab <bassimstudy@gmail.com>
2026-03-06 05:24:31 +0200

    feat(TaskService): Add AddUserToQuestDto with required properties.

---
+++ Microservices/TaskService/Core/Dtos/AddUserToQuestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Dtos;
+
+public class AddUserToQuestDto
+{
+    [Required(ErrorMessage = "UserId is required.")]
+    public string UserId { get; set; } = default!;
+    [Required(ErrorMessage = "QuestId is required.")]
+    public string QuestId { get; set; } = default!;
+    [Required(ErrorMessage = "TeamId is required.")]
+    public string TeamId { get; set; } = default!;
+}
