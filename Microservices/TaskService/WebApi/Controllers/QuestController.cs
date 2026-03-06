7dc8f31707c1543ccd578db2695d21893c56e186
BassimEhab <bassimstudy@gmail.com>
2026-03-06 02:19:11 +0200

    feat(TaskService): Add QuestController with POST endpoint for creating tasks.

---
+++ Microservices/TaskService/WebApi/Controllers/QuestController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Dtos;
+using Core.Result;
+using Core.ServicesAbstractions;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/Tasks")]
+public class QuestController(IQuestService questService,
+                             IValidator<QuestDto> questDtoValidator) : BaseApiController
+{
+    [HttpPost("{planId}/task")]
+    [ProducesResponseType(typeof(QuestToReturnDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<QuestToReturnDto>> CreateTask([FromBody] QuestDto questDto, string planId)
+    {
+        var validationResult = await ExecuteValidation(questDtoValidator, questDto);
+        if (!validationResult.IsSuccess)
+            return BadRequest(validationResult.ErrorList);
+        var result = await questService.CreateQuestAsync(questDto, planId);
+        return result.Map<ActionResult<QuestToReturnDto>>(
+            onSuccess: quest => Ok(quest),
+            onFailure: err => HandleFailure(err));
+    }
+}
