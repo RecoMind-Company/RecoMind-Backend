1c2701fbf720553ec0b00cad0bf75a6510165c19
BassimEhab <bassimstudy@gmail.com>
2026-03-07 04:28:04 +0200

    feat(TaskService): Add JWT auth support and Swagger security enhancements.
    Added JwtOptions class for JWT config. Integrated JWT authentication in Program.cs, including token validation and reading options from config. Enhanced Swagger setup to support Bearer authentication.

--- Microservices/TaskService/WebApi/Program.cs
+++ Microservices/TaskService/WebApi/Program.cs
@@ -2,10 +2,15 @@ using Core.Interfaces;
 using Core.MappingProfiles;
 using Core.Services;
 using Core.ServicesAbstractions;
+using Core.Settings;
 using FluentValidation;
 using Infrastructure.Context;
 using Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using System.Text.Json.Serialization;
 using WebApi.Validators;
 
@@ -14,7 +19,47 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddJsonOptions(jo => jo.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(cfg =>
+{
+    cfg.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+    cfg.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "BearerAuth" }
+            },
+            []
+        }
+    });
+});
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var jwt = builder.Configuration.GetSection("JwtOptions").Get<JwtOptions>();
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwt.Issuer,
+        ValidAudience = jwt.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.SecretKey)),
+    };
+});
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
