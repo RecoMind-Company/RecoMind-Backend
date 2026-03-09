897dfc076e782e6ac3bfce54a8fd03e370c1cfdc
BassimEhab <bassimstudy@gmail.com>
2026-03-09 06:22:37 +0200

    refactor(TaskService): Make Quest property nullable in UserQuests class.

--- Microservices/TaskService/Core/Models/UserQuests.cs
+++ Microservices/TaskService/Core/Models/UserQuests.cs
@@ -4,5 +4,5 @@ public class UserQuests
 {
     public string QuestId { get; set; } = default!;
     public string UserId { get; set; } = default!;
-    public Quest Quest { get; set; } = default!;
+    public Quest? Quest { get; set; } = default!;
 }
