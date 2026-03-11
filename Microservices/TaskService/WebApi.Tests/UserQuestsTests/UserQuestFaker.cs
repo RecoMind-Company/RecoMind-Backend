b23c7cf42628bfb9ec8c3413c8cd2c03c46d6d63
BassimEhab <bassimstudy@gmail.com>
2026-03-11 08:09:46 +0200

    feat(TaskService): Add UserQuestFaker for generating test UserQuests.

---
+++ Microservices/TaskService/WebApi.Tests/UserQuestsTests/UserQuestFaker.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Core.Models;
+
+namespace WebApi.Tests.UserQuestsTests;
+
+internal class UserQuestFaker
+{
+    internal static Faker<UserQuests> UserQuests(int seed = 0) => new Faker<UserQuests>()
+        .Rules((f, uq) =>
+        {
+            uq.QuestId = f.Random.Guid().ToString();
+            uq.UserId = f.Random.Guid().ToString();
+            uq.Quest = null;
+        });
+}
