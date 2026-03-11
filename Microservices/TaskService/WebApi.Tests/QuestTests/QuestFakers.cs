6da72437abbd83f921dbd1a7ba47c86f3c74fb25
BassimEhab <bassimstudy@gmail.com>
2026-03-11 19:25:39 +0200

    feat(TaskService): Add UpdateQuestDto faker with valid and invalid rules.
    Added internal static method UpdateQuestDto to QuestFakers for generating fake UpdateQuestDto objects. Includes randomized field values and an "InValid" ruleset for testing invalid data scenarios.

--- Microservices/TaskService/WebApi.Tests/QuestTests/QuestFakers.cs
+++ Microservices/TaskService/WebApi.Tests/QuestTests/QuestFakers.cs
@@ -48,4 +48,22 @@ internal static class QuestFakers
             q.QuestId = f.Random.Guid().ToString();
             q.UserAssignedQuests = [];
         });
+    internal static Faker<UpdateQuestDto> UpdateQuestDto(int seed = 0) => new Faker<UpdateQuestDto>()
+        .UseSeed(seed)
+        .Rules((f, uq) =>
+        {
+            uq.Title = f.Random.Bool(0.5f) ? f.Lorem.Sentence(3) : null;
+            uq.Description = f.Random.Bool(0.5f) ? f.Lorem.Letter(20) : null;
+            uq.Status = f.Random.Bool(0.5f) ? (int?)f.PickRandom<QuestStatusEnum>() : null;
+            uq.StartDate = seed % 2 == 0 ? null : f.Date.Future(1, DateTime.UtcNow.AddSeconds(10));
+            uq.DeadLine = seed % 2 == 0 ? null : uq.StartDate!.Value.AddDays(1);
+        })
+        .RuleSet(InValid, rules =>
+        {
+            rules.RuleFor(q => q.Title, f => f.Lorem.Letter(300));
+            rules.RuleFor(q => q.Description, f => f.Lorem.Letter(300));
+            rules.RuleFor(q => q.Status, f => 5);
+            rules.RuleFor(q => q.StartDate, f => DateTime.UtcNow.AddDays(-5));
+            rules.RuleFor(q => q.DeadLine, f => DateTime.UtcNow.AddDays(-6));
+        });
 }
