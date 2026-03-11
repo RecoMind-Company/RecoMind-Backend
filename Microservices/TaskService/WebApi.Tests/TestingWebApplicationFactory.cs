415c44a7efb436cf870104880535e644b363d5a0
BassimEhab <bassimstudy@gmail.com>
2026-03-11 06:32:22 +0200

    feat(TaskService): Add WebApi.Tests project with test utilities and setup.
    Added WebApi.Tests to solution with .NET 8 test project, dependencies (xUnit, FluentAssertions, coverlet, Bogus, EF Core Sqlite, MVC Testing), and project reference to WebApi. Introduced  TestingWebApplicationFactory for integration tests using in-memory SQLite. Updated Task.sln for project inclusion and build configs.

---
+++ Microservices/TaskService/WebApi.Tests/TestingWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Tests;
+
+public class TestingWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlite(connection);
+            });
+        });
+    }
+}
