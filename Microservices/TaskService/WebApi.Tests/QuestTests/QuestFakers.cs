d723a998baf652b55afd1002b440e4fac6f0b8b8
BassimEhab <bassimstudy@gmail.com>
2026-03-11 08:09:00 +0200

    refactor(TaskService): Refactor QuestFakers: internal access, method renames.

--- Microservices/TaskService/WebApi.Tests/QuestTests/QuestFakers.cs
+++ Microservices/TaskService/WebApi.Tests/QuestTests/QuestFakers.cs
@@ -4,10 +4,10 @@ using Core.Models;
 
 namespace WebApi.Tests.QuestTests;
 
-public static class QuestFakers
+internal static class QuestFakers
 {
-    public const string InValid = "InValid";
-    public static Faker<QuestDto> CreateQuestDto(int seed = 0) => new Faker<QuestDto>()
+    internal const string InValid = "InValid";
+    internal static Faker<QuestDto> QuestDto(int seed = 0) => new Faker<QuestDto>()
         .UseSeed(seed)
         .RuleFor(q => q.Title, f => f.Lorem.Sentence(3))
         .RuleFor(q => q.Description, f => seed % 2 == 0 ? f.Lorem.Letter(20) : null)
@@ -24,10 +24,10 @@ public static class QuestFakers
             rules.RuleFor(q => q.DeadLine, f => DateTime.UtcNow.AddDays(-6));
         });
 
-    public static Faker<QuestToReturnDto> CreateQuestToRetrunDto(int seed = 0) => new Faker<QuestToReturnDto>()
+    internal static Faker<QuestToReturnDto> QuestToRetrunDto(int seed = 0) => new Faker<QuestToReturnDto>()
         .UseSeed(seed)
         .RuleFor(q => q.Title, f => f.Lorem.Sentence(3))
-        .RuleFor(q => q.Description, f => seed % 2 == 0 ? f.Lorem.Letter(200) : null)
+        .RuleFor(q => q.Description, f => seed % 2 == 0 ? f.Lorem.Letter(20) : null)
         .RuleFor(q => q.Status, f => seed % 2 == 0 ? f.PickRandom<QuestStatusEnum>() : QuestStatusEnum.active)
         .RuleFor(q => q.StartDate, f => seed % 2 == 0 ? f.Date.Future(1, DateTime.UtcNow.AddSeconds(10)) : DateTime.UtcNow)
         .RuleFor(q => q.DeadLine, (f, q) => seed % 2 == 0 ? q.StartDate.AddDays(1) : DateTime.UtcNow.AddDays(1))
@@ -35,4 +35,17 @@ public static class QuestFakers
         .RuleFor(q => q.Duration, (f, q) => q.DeadLine - q.StartDate)
         .RuleFor(q => q.PlanId, f => "plan1")
         .RuleFor(q => q.UserAssignedQuests, f => []);
+    internal static Faker<Quest> Quest(int seed = 0) => new Faker<Quest>()
+         .UseSeed(seed)
+        .Rules((f, q) =>
+        {
+            q.Title = f.Lorem.Sentence(3);
+            q.Description = f.Random.Bool() ? f.Lorem.Letter(20) : null;
+            q.Status = f.PickRandom<QuestStatusEnum>();
+            q.StartDate = f.Date.Future(1, DateTime.UtcNow.AddSeconds(10));
+            q.DeadLine = q.StartDate.AddDays(1);
+            q.PlanId = "plan1";
+            q.QuestId = f.Random.Guid().ToString();
+            q.UserAssignedQuests = [];
+        });
 }
