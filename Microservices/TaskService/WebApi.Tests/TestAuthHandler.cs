17962597569abf0426432b0a0067b2a0096263be
BassimEhab <bassimstudy@gmail.com>
2026-03-12 04:01:06 +0200

    feat(TaskService): Add custom test authentication handler for integration tests.
    Added TestAuthHandler.cs to enable authentication in test scenarios by checking for a "Test-Authorization" header and generating user claims. Returns no authentication result if header is missing, triggering standard 401 responses.

---
+++ Microservices/TaskService/WebApi.Tests/TestAuthHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace WebApi.Tests;
+
+internal class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (!Context.Request.Headers.TryGetValue("Test-Authorization", out var userId) || string.IsNullOrEmpty(userId.FirstOrDefault()))
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        var claims = new[]
+        {
+        new Claim(ClaimTypes.NameIdentifier, userId.First()!),
+        new Claim(ClaimTypes.Name, "Test User")
+        };
+        var identity = new ClaimsIdentity(claims, "TestSchema");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestSchema");
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
