0033988d08d68fbd7bbef14ebe3ab980aa396041
BassimEhab <bassimstudy@gmail.com>
2026-03-12 04:06:53 +0200

    feat(TaskService): Add AddUserToTaskAsync tests.
    Added UserQuestsControllerTests.cs with comprehensive integration tests for UserQuests API, covering valid, invalid, not-found, and duplicate user cases.
     Replaced UserQuestFaker.cs with UserQuestFakers.cs for improved and consolidated test data generation, including invalid data scenarios.

---
+++ Microservices/TaskService/WebApi.Tests/UserQuestsTests/UserQuestFakers.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using Core.Dtos;
+using Core.Models;
+
+namespace WebApi.Tests.UserQuestsTests;
+
+internal class UserQuestFakers
+{
+    internal const string InValid = "InValid";
+    internal static Faker<UserQuests> UserQuests(int seed = 0) => new Faker<UserQuests>()
+        .UseSeed(seed)
+        .Rules((f, uq) =>
+        {
+            uq.QuestId = f.Random.Guid().ToString();
+            uq.UserId = f.Random.Guid().ToString();
+            uq.Quest = null;
+        });
+    internal static Faker<AddUserToQuestDto> AddUserToQuest(int seed = 0) => new Faker<AddUserToQuestDto>()
+        .UseSeed(seed)
+        .Rules((f, auq) =>
+        {
+            auq.QuestId = f.Random.Guid().ToString();
+            auq.UserId = f.Random.Guid().ToString();
+            auq.TeamId = f.Random.Guid().ToString();
+        })
+        .RuleSet(InValid, ruleset =>
+        {
+            ruleset.RuleFor(auq => auq.QuestId, f => null);
+            ruleset.RuleFor(auq => auq.UserId, f => null);
+            ruleset.RuleFor(auq => auq.TeamId, f => null);
+        });
+}
