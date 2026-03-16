3c2f70d25ef08941a105d51ea3ffb2a0af43fd89
BassimEhab <bassimstudy@gmail.com>
2026-03-16 23:32:30 +0200

    feat(CommentService): Add Result pattern with error and result handling classes.

---
+++ Microservices/CommentService/Core/Result/CommentErrors.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.Result;
+
+internal class CommentErrors
+{
+}
---
+++ Microservices/CommentService/Core/Result/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Result;
+
+public class Result<T>
+{
+    public IEnumerable<Error> ErrorsList { get; set; } = [];
+    public T Value { get; set; }
+    public bool IsSuccess => !ErrorsList.Any();
+    private Result(T value)
+    {
+        Value = value;
+    }
+    private Result(IEnumerable<Error> errors) => ErrorsList = errors;
+    private Result(Error error) => ErrorsList = new List<Error> { error };
+    public Result<T> Success(T value) => new Result<T>(value);
+    public Result<T> Failure(Error error) => new Result<T>(error);
+    public Result<T> Failure(IEnumerable<Error> errors) => new Result<T>(errors);
+    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onFailure) => IsSuccess ? onSuccess(Value) : onFailure(ErrorsList);
+}
