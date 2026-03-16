10230df6ae991ffd55ef5d6300ed475c0996efd8
BassimEhab <bassimstudy@gmail.com>
2026-03-16 23:35:46 +0200

    feat(CommentService): Add comment model and ApplicationDbContext.

---
+++ Microservices/CommentService/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Comment> Comments { get; set; } = default!;
+}
