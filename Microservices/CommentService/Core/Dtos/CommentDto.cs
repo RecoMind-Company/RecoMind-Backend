2514bcce282b89844da78cb54f9d4858ec3acee0
BassimEhab <bassimstudy@gmail.com>
2026-03-19 01:27:30 +0200

    feat(CommentService): Add comment DTOs and AutoMapper profile.

---
+++ Microservices/CommentService/Core/Dtos/CommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Dtos;
+
+public class CommentDto
+{
+    public string Id { get; set; } = default!;
+    public string UserComment { get; set; } = default!;
+    public string UserId { get; set; } = default!;
+}
---
+++ Microservices/CommentService/Core/MappingProfiles/CommentProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Dtos;
+using Core.Models;
+
+namespace Core.MappingProfiles;
+
+public class CommentProfile : Profile
+{
+    public CommentProfile()
+    {
+        CreateMap<AddCommentDto, Comment>()
+            .ForMember(des => des.Id, opt => opt.MapFrom(_ => Guid.NewGuid().ToString()));
+
+        CreateMap<Comment, CommentDto>();
+    }
+
+
+}
