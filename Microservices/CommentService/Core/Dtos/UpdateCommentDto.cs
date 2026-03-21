071614f953a332506807a46f807b4658d805af03
BassimEhab <bassimstudy@gmail.com>
2026-03-21 21:07:41 +0200

    feat(CommentService): Add UpdateCommentDto and update AutoMapper profile.
    Added UpdateCommentDto with UserComment and JsonIgnore properties for CommentId and UserId. Updated CommentProfile to map UpdateCommentDto to Comment, conditionally updating UserComment and ignoring Id, UserId, and PlanId during mapping.

---
+++ Microservices/CommentService/Core/Dtos/UpdateCommentDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.Dtos;
+
+public class UpdateCommentDto
+{
+    public string? UserComment { get; set; }
+    [JsonIgnore]
+    public string? CommentId { get; set; }
+    [JsonIgnore]
+    public string? UserId { get; set; }
+}
--- Microservices/CommentService/Core/MappingProfiles/CommentProfile.cs
+++ Microservices/CommentService/Core/MappingProfiles/CommentProfile.cs
@@ -12,6 +12,16 @@ public class CommentProfile : Profile
             .ForMember(des => des.Id, opt => opt.MapFrom(_ => Guid.NewGuid().ToString()));
 
         CreateMap<Comment, CommentDto>();
+
+        CreateMap<UpdateCommentDto, Comment>()
+            .ForMember(des => des.UserComment, opt =>
+            {
+                opt.Condition(src => !string.IsNullOrEmpty(src.UserComment));
+                opt.MapFrom(src => src.UserComment);
+            })
+            .ForMember(des => des.Id, opt => opt.Ignore())
+            .ForMember(des => des.UserId, opt => opt.Ignore())
+            .ForMember(des => des.PlanId, opt => opt.Ignore());
     }
 
 
