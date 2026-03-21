682ad8d902a4dc980a792140e88609ce6f2052b6
BassimEhab <bassimstudy@gmail.com>
2026-03-21 19:11:25 +0200

    refactor(CommentService): Refactor Result<T> for clarity.
    - Make Value property nullable to represent failures
    - Use expression-bodied members and collection expressions
    - Add comments to clarify code structure
    - Apply null-forgiving operator in Map method
    - Modernize code for improved readability and maintainability

--- Microservices/CommentService/Core/Result/Result.cs
+++ Microservices/CommentService/Core/Result/Result.cs
@@ -3,16 +3,16 @@
 public class Result<T>
 {
     public IEnumerable<Error> ErrorsList { get; set; } = [];
-    public T Value { get; set; }
+    public T? Value { get; set; }
     public bool IsSuccess => !ErrorsList.Any();
-    private Result(T value)
-    {
-        Value = value;
-    }
+    // constractors
+    private Result(T value) => Value = value;
     private Result(IEnumerable<Error> errors) => ErrorsList = errors;
-    private Result(Error error) => ErrorsList = new List<Error> { error };
-    public static Result<T> Success(T value) => new Result<T>(value);
-    public static Result<T> Failure(Error error) => new Result<T>(error);
-    public static Result<T> Failure(IEnumerable<Error> errors) => new Result<T>(errors);
-    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onFailure) => IsSuccess ? onSuccess(Value) : onFailure(ErrorsList);
+    private Result(Error error) => ErrorsList = [error];
+    // factory methods
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(Error error) => new(error);
+    public static Result<T> Failure(IEnumerable<Error> errors) => new(errors);
+    // mapping method
+    public TResult Map<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onFailure) => IsSuccess ? onSuccess(Value!) : onFailure(ErrorsList);
 }
