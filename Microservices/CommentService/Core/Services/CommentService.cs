01ebaddf5894da0032a23bb4e2d2d69a80b7577c
BassimEhab <bassimstudy@gmail.com>
2026-03-21 19:29:50 +0200

    feat(CommentService): Add GetCommentsByPlanIdAsync to comment service.
    Implemented GetCommentsByPlanIdAsync in CommentService to fetch comments by plan ID and return them as CommentDto results. Updated ICommentService interface to include the new method.

--- Microservices/CommentService/Core/Services/CommentService.cs
+++ Microservices/CommentService/Core/Services/CommentService.cs
@@ -21,6 +21,12 @@ public class CommentService(IUnitOfWork unitOfWork,
         await unitOfWork.SaveChangesAsync();
         var commentDto = mapper.Map<CommentDto>(comment);
         return Result<CommentDto>.Success(commentDto);
-
     }
+    public async Task<Result<IEnumerable<CommentDto>>> GetCommentsByPlanIdAsync(string planId)
+    {
+        var comments = await _commentRepository.FindAll(c => c.PlanId == planId);
+        var commentDtos = mapper.Map<IEnumerable<CommentDto>>(comments);
+        return Result<IEnumerable<CommentDto>>.Success(commentDtos);
+    }
+
 }
--- Microservices/CommentService/Core/ServicesAbstraction/ICommentService.cs
+++ Microservices/CommentService/Core/ServicesAbstraction/ICommentService.cs
@@ -6,4 +6,5 @@ namespace Core.ServicesAbstraction;
 public interface ICommentService
 {
     Task<Result<CommentDto>> AddCommentAsync(AddCommentDto addCommentDto);
+    Task<Result<IEnumerable<CommentDto>>> GetCommentsByPlanIdAsync(string planId);
 }
