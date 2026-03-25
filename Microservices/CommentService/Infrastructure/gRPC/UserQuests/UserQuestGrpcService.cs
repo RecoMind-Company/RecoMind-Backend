439948fa32099bc994ad5bcb46f92f80c04e53d1
BassimEhab <bassimstudy@gmail.com>
2026-03-25 22:03:27 +0200

    feat(CommentService): Add gRPC service to check if user is in a plan.
    Introduced IUserQuestGrpcService interface and implemented UserQuestGrpcService using gRPC. Added proto files for user-plan check, updated Infrastructure.csproj for gRPC code generation, and integrated the new service for plan membership verification.

---
+++ Microservices/CommentService/Infrastructure/gRPC/UserQuests/UserQuestGrpcService.cs
@@ -0,0 +1,18 @@
+﻿using Core.ServicesAbstraction;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.gRPC.UserQuests;
+
+public class UserQuestGrpcService(GrpcUserQuestsService.GrpcUserQuestsServiceClient serviceClient, ILogger<UserQuestGrpcService> logger) : IUserQuestGrpcService
+{
+    public async Task<bool> IsUserInPlan(string userId, string planId)
+    {
+        var request = new IsUserInPlanRequest
+        {
+            UserId = userId,
+            PlanId = planId
+        };
+        var response = await serviceClient.IsUserInAnyQuestWithinPlanAsync(request);
+        return response.IsInPlan;
+    }
+}
