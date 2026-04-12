55300081218837ac0edeeba256f4407e5f1fa2c8
BassimEhab <bassimstudy@gmail.com>
2026-04-12 20:27:05 +0200

    feat(TaskService): Add UserNotInTeam error to UserQuestsErrors class.

--- Microservices/TaskService/Core/Result/UserQuestsErrors.cs
+++ Microservices/TaskService/Core/Result/UserQuestsErrors.cs
@@ -6,4 +6,6 @@ public class UserQuestsErrors
        new("Task.UserAlreadyAssigned", "The user is already assigned to this Task.");
     public static Error UserNotAssignedToQuest =>
         new("Task.UserNotAssigned", "This user is not assigned to this Task.");
+    public static Error UserNotInTeam =>
+        new("Task.UserNotInTeam", "This user is not in the team.");
 }
