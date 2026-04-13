775d2ed4504aafa5068405301bf5fb8924972f56
BassimEhab <bassimstudy@gmail.com>
2026-04-13 09:11:38 +0200

    feat(CommentService): Enable gRPC services and add Team service support.

---
+++ Microservices/CommentService/Infrastructure/gRPC/Team/GrpcTeamService.cs
@@ -0,0 +1,19 @@
+﻿using Core.ServicesAbstraction;
+
+namespace Infrastructure.gRPC.Team;
+
+public class GrpcTeamService(TeamGrpcService.TeamGrpcServiceClient teamGrpcServiceClient) : IGrpcTeamService
+{
+    public async Task<bool> IsUserExist(string userId, string teamId)
+    {
+        var request = new UserExistRequest
+        {
+            UserId = userId,
+            TeamId = teamId
+        };
+
+        var result = await teamGrpcServiceClient.UserExistAsync(request);
+
+        return result.Exist;
+    }
+}
