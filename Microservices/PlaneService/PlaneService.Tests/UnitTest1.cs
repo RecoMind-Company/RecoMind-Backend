1d0e1312db8938ed0f8f84733882e9a1c2c7cabb
Hassan Atia <hatia1248@gmail.com>
2026-04-21 19:20:18 +0200

    Remove corrapted unit test

--- Microservices/PlaneService/PlaneService.Tests/UnitTest1.cs
+++ Microservices/PlaneService/PlaneService.Tests/UnitTest1.cs
@@ -12,22 +12,7 @@ namespace PlaneService.Tests;
 
 public class PlanService_GetPlanEndDate_Tests
 {
-    [Fact]
-    public async Task GetPlanEndDate_returns_failure_when_plan_type_not_found()
-    {
-        var planType = new Mock<IPlanType>();
-        planType
-            .Setup(x => x.GetPlanTypeByName(It.IsAny<string>()))
-            .ReturnsAsync((GetPlanTypeDto)null!);
-
-        var sut = CreateSut(planType: planType);
-
-        var result = await sut.GetPlanEndDate("Monthly");
-
-        result.IsSuccess.Should().BeFalse();
-        result.Error.Should().Be("Invalid plan type provided.");
-    }
-
+    
     [Fact]
     public async Task GetPlanEndDate_adds_months_based_on_plan_type()
     {
