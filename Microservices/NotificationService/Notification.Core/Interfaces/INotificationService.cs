fffefbc01c30d1f6d8bbc923289108ffc87495d3
ahmedMo77 <a7medmo25@gmail.com>
2026-04-30 00:36:02 +0300

    Refactoring with add result pattern to Notification

--- Microservices/NotificationService/Notification.Core/Interfaces/INotificationService.cs
+++ Microservices/NotificationService/Notification.Core/Interfaces/INotificationService.cs
@@ -1,5 +1,6 @@
 ﻿using Notification.Core.DTOs;
 using Notification.Core.Models;
+using Notification.Core.Result;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,12 @@ namespace Notification.Core.Interfaces
 {
     public interface INotificationService
     {
-        Task SendNotificationAsync(NotificationEventDto notification);
-        Task<IEnumerable<NotificationResponseDto>> GetUserHistoryAsync(string userId);
-        Task<IEnumerable<NotificationResponseDto>> GetByStatusAsync(string userId, bool isRead);
-        Task<NotificationResponseDto?> GetAndMarkAsReadAsync(string id);
-        Task<bool> MarkAllAsReadAsync(string userId);
-        Task<bool> DeleteNotificationAsync(string id);
-        Task<int> GetUnreadCountAsync(string userId);
+        Task<Result<bool>> SendNotificationAsync(NotificationEventDto notification);
+        Task<Result<IEnumerable<NotificationResponseDto>>> GetUserHistoryAsync(string userId);
+        Task<Result<IEnumerable<NotificationResponseDto>>> GetByStatusAsync(string userId, bool isRead);
+        Task<Result<NotificationResponseDto>> GetAndMarkAsReadAsync(string id);
+        Task<Result<bool>> MarkAllAsReadAsync(string userId);
+        Task<Result<bool>> DeleteNotificationAsync(string id);
+        Task<Result<int>> GetUnreadCountAsync(string userId);
     }
 }
--- Microservices/NotificationService/Notification.Core/Result/Error.cs
+++ Microservices/NotificationService/Notification.Core/Result/Error.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Team.Core.Result
+namespace Notification.Core.Result
 {
     public record Error(string Code, string Message);
+
 }
