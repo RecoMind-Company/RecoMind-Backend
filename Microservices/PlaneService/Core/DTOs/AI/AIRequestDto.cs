61fef7a35d7aeda988b87c4597f91a939e551bf7
BassimEhab <bassimstudy@gmail.com>
2026-06-26 22:47:46 +0300

    feat(Plan): Add AIRequestDto.

---
+++ Microservices/PlaneService/Core/DTOs/AI/AIRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.DTOs.AI;
+
+public class AIRequestDto
+{
+    [JsonPropertyName("company_id")]
+    public string company_id { get; set; }
+    [JsonPropertyName("plan_text")]
+    public string plan_text { get; set; }
+    [JsonPropertyName("team_id")]
+    public string team_id { get; set; }
+}
