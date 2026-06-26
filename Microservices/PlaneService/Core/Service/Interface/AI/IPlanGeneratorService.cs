afd0f78ef2200f02f34268851eda307346cb540e
BassimEhab <bassimstudy@gmail.com>
2026-06-26 22:55:13 +0300

    feat(Plan): Add AI plan generator service interface and implementation.

---
+++ Microservices/PlaneService/Core/Service/Interface/AI/IPlanGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using Core.DTOs.AI;
+using Core.Models;
+
+namespace Core.Service.Interface.AI;
+
+public interface IPlanGeneratorService
+{
+    Task<Result<AIResultDto>> GeneratePlan(AIRequestDto requestDto);
+}
---
+++ Microservices/PlaneService/Infrastructure/AI/PlanGeneratorService.cs
@@ -0,0 +1,24 @@
+﻿using Core.DTOs.AI;
+using Core.Models;
+using Core.Service.Interface.AI;
+using System.Net.Http.Json;
+
+namespace Infrastructure.AI;
+
+public class PlanGeneratorService(HttpClient httpClient) : IPlanGeneratorService
+{
+    public async Task<Result<AIResultDto>> GeneratePlan(AIRequestDto RequestDto)
+    {
+        var response = await httpClient.PostAsJsonAsync("ROUTE_TO_AI_SERVICE", RequestDto);
+        try
+        {
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<AIResultDto>();
+            return Result<AIResultDto>.Success(result!);
+        }
+        catch (Exception)
+        {
+            return Result<AIResultDto>.Failure($"Failed to generate plan because {response.Content}");
+        }
+    }
+}
