136ba9273e4cab196f164117e06a186e0b120cf6
BassimEhab <bassimstudy@gmail.com>
2026-06-29 03:23:31 +0300

    feat(Comment): Add Quest grpc to check quest (task) existince, with it's configuration.

---
+++ Microservices/CommentService/Core/ServicesAbstraction/IQuestGrpcService.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.ServicesAbstraction;
+
+public interface IQuestGrpcService
+{
+    Task<bool> IsTaskExists(string taskId);
+}
---
+++ Microservices/CommentService/Infrastructure/gRPC/Quest/QuestGrpcService.cs
@@ -0,0 +1,14 @@
+﻿using Core.ServicesAbstraction;
+using Infrastructure.gRPC.Quest;
+
+namespace WebApi.gRPC.Quest;
+
+public class QuestGrpcService(GrpcQuestsService.GrpcQuestsServiceClient grpcQuestsServiceClient) : IQuestGrpcService
+{
+    public async Task<bool> IsTaskExists(string taskId)
+    {
+        var request = new IsTaskExistsRequest { TaskId = taskId };
+        var response = await grpcQuestsServiceClient.IsTaskExistsAsync(request);
+        return response.IsExists;
+    }
+}
