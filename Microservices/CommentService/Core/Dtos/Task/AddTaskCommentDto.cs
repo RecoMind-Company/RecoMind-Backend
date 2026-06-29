9a58701c19e58ef7b42701155b2e6e05a552124a
BassimEhab <bassimstudy@gmail.com>
2026-06-29 15:41:41 +0300

    feat(Comment): Add task comment dtos.

---
+++ Microservices/CommentService/Core/Dtos/Task/AddTaskCommentDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.Dtos.Task;
+
+public class AddTaskCommentDto
+{
+    public string? UserComment { get; set; }
+    [JsonIgnore]
+    public string? QuestId { get; set; }
+    [JsonIgnore]
+    public string? UserId { get; set; }
+}
---
+++ Microservices/CommentService/Core/Dtos/Task/TaskCommentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Dtos.Task;
+
+public class TaskCommentDto
+{
+    public string Id { get; set; } = default!;
+    public string UserComment { get; set; } = default!;
+    public string UserId { get; set; } = default!;
+    public string QuestId { get; set; } = default!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsUpdated => UpdatedAt.HasValue;
+}
