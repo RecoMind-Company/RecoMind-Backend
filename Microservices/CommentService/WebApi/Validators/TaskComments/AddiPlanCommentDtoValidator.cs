09c478f27ffbed2037a4adc778cb70e74d286e30
BassimEhab <bassimstudy@gmail.com>
2026-06-29 16:42:54 +0300

    feat(Comment): Add Task comment validator.

---
+++ Microservices/CommentService/WebApi/Validators/TaskComments/AddiPlanCommentDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using Core.Dtos.Task;
+using FluentValidation;
+
+namespace WebApi.Validators.TaskComments;
+
+internal class AddTaskCommentDtoValidator : AbstractValidator<AddTaskCommentDto>
+{
+    public AddTaskCommentDtoValidator()
+    {
+        RuleFor(x => x.UserComment)
+            .NotEmpty().WithMessage("User comment is required.")
+            .MaximumLength(500);
+
+    }
+}
---
+++ Microservices/CommentService/WebApi/Validators/TaskComments/UpdatePlanCommentDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Dtos.Task;
+using FluentValidation;
+
+
+namespace WebApi.Validators.TaskComments;
+
+internal class UpdateTaskCommentDtoValidator : AbstractValidator<UpdateTaskCommentDto>
+{
+    public UpdateTaskCommentDtoValidator()
+    {
+        RuleFor(x => x.UserComment)
+            .MaximumLength(500)
+            .WithMessage("User comment must not exceed 500 characters.")
+            .When(x => !string.IsNullOrEmpty(x.UserComment));
+
+        RuleFor(x => x.CommentId)
+            .NotEmpty()
+            .WithMessage("Comment ID is required.");
+    }
+}
+
