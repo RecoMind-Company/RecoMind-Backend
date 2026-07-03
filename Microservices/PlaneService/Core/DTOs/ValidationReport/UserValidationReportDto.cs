a4121db914d6dece509b25630e0b7f42db80cd20
BassimEhab <bassimstudy@gmail.com>
2026-07-03 18:31:40 +0300

    fix(Plan): fix add Validation Report to return good structured data.

--- Microservices/PlaneService/Core/DTOs/ValidationReport/UserValidationReportDto.cs
+++ Microservices/PlaneService/Core/DTOs/ValidationReport/UserValidationReportDto.cs
@@ -1,11 +1,12 @@
-﻿using Core.Models;
+﻿using Core.DTOs.AI.ValidationReport.AIResult;
+using Core.Models;
 
 namespace Core.DTOs.ValidationReport;
 
 public class UserValidationReportDto
 {
     public string Id { get; set; } = default!;
-    public string Content { get; set; }
+    public ValidationReportDto Content { get; set; }
     public string CreatedBy { get; set; } = default!;
     public DateTime CreatedAt { get; set; }
     public ValidationReportStatusEnum Status { get; set; }
